8026abfd054f0bd26e313ff3722e01b71b6564ab
lizandreina <156568298+lizandreina@users.noreply.github.com>
2024-07-10 12:29:31 +0200

    Add functions for basic math operators

---
+++ script.js
@@ -0,0 +1,15 @@
+function sum(first, second) {
+    return first + second;
+}
+
+function substract(first, second) {
+    return first - second;
+}
+
+function multiply(first, second) {
+    return first * second;
+}
+
+function divide(first, second) {
+    return first / second;
+}
