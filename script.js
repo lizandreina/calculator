a60bae9af63f5e23fbc9ba61ea198c3d7e3b8b1c
lizandreina <156568298+lizandreina@users.noreply.github.com>
2024-07-10 12:31:36 +0200

    Create the function that runs the operation

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ function multiply(first, second) {
 function divide(first, second) {
     return first / second;
 }
+
+let first = 0;
+let second = 0;
+let operator = 0;
+
+function operate(operator, one, two){
+    first = one;
+    second = two;
+    if (operator === '+') {
+        sum(first, second);
+    } else if (operator === '-') {
+        substract(first, second);
+    } else if (operator === '*') {
+        multiply(first, second);
+    } else if (operator === '/') {
+        divide(first, second);
+    }
+}
