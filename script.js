f21d694d7bacf7627349c1005249af98c67762cb
lizandreina <156568298+lizandreina@users.noreply.github.com>
2024-07-10 14:24:56 +0200

    Make the calculator work

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 function sum(first, second) {
-    return first + second;
+    display = first + second;
+    return display;
 }
 
 function substract(first, second) {
-    return first - second;
+    display = first - second;
+    return display;
 }
 
 function multiply(first, second) {
-    return first * second;
+    display = first * second;
+    return display;
 }
 
 function divide(first, second) {
-    return first / second;
+    display = first / second;
+    return display;
 }
 
 let first = 0;
@@ -19,27 +23,158 @@ let second = 0;
 let operator = 0;
 
 function operate(operator, one, two){
-    first = one;
-    second = two;
-    if (operator === '+') {
-        sum(first, second);
-    } else if (operator === '-') {
-        substract(first, second);
-    } else if (operator === '*') {
-        multiply(first, second);
-    } else if (operator === '/') {
-        divide(first, second);
+    if (operator === 'plus') {
+        sum(one, two);
+    } else if (operator === 'minus') {
+        substract(one, two);
+    } else if (operator === 'multiply') {
+        multiply(one, two);
+    } else if (operator === 'divide') {
+        divide(one, two);
     }
 }
 
-let display = '';
-
+let display = 0;
 const screen = document.querySelector('.display');
-const digits = document.querySelectorAll('.digit');
 
-digits.forEach((button) => {
-    button.addEventListener("click", (e) => {
-        display = e.target.innerText;
+let clicked = 0;
+let keyboard = document.querySelector('.keyboard');
+
+keyboard.addEventListener('click', (event) => {
+    let target = event.target;
+
+    switch(target.className) {
+    case 'digit cero':
+        display = 0;
         screen.textContent = display;
-    });
-  });
+        clicked++;
+        if (clicked === 1) {
+            first = 0;
+        } else if (clicked === 2) {
+            second = 0;
+            clicked = 0;
+        }
+        break;
+    case 'digit one':
+        display = 1;
+        screen.textContent = display;
+        clicked++;
+        if (clicked === 1) {
+            first = 1;
+        } else if (clicked === 2) {
+            second = 1;
+            clicked = 0;
+        }
+        break;
+    case 'digit two':
+        display = 2;
+        screen.textContent = display;
+        clicked++;
+        if (clicked === 1) {
+            first = 2;
+        } else if (clicked === 2) {
+            second = 2;
+            clicked = 0;
+        }
+        break;
+    case 'digit three':
+        display = 3;
+        screen.textContent = display;
+        clicked++;
+        if (clicked === 1) {
+            first = 3;
+        } else if (clicked === 2) {
+            second = 3;
+            clicked = 0;
+        }
+        break;
+    case 'digit four':
+        display = 4;
+        screen.textContent = display;
+        clicked++;
+        if (clicked === 1) {
+            first = 4;
+        } else if (clicked === 2) {
+            second = 4;
+            clicked = 0;
+        }
+        break;
+    case 'digit five':
+        display = 5;
+        screen.textContent = display;
+        clicked++;
+        if (clicked === 1) {
+            first = 5;
+        } else if (clicked === 2) {
+            second = 5;
+            clicked = 0;
+        }
+        break;
+    case 'digit six':
+        display = 6;
+        screen.textContent = display;
+        clicked++;
+        if (clicked === 1) {
+            first = 6;
+        } else if (clicked === 2) {
+            second = 6;
+            clicked = 0;
+        }
+        break;
+    case 'digit seven':
+        display = 7;
+        screen.textContent = display;
+        clicked++;
+        if (clicked === 1) {
+            first = 7;
+        } else if (clicked === 2) {
+            second = 7;
+            clicked = 0;
+        }
+        break;
+    case 'digit eight':
+        display = 8;
+        screen.textContent = display;
+        clicked++;
+        if (clicked === 1) {
+            first = 8;
+        } else if (clicked === 2) {
+            second = 8;
+            clicked = 0;
+        }
+        break;
+    case 'digit nine':
+        display = 9;
+        screen.textContent = display;
+        clicked++;
+        if (clicked === 1) {
+            first = 9;
+        } else if (clicked === 2) {
+            second = 9;
+            clicked = 0;
+        }
+        break;
+    case 'symbol AC':
+        first = 0;
+        second = 0;
+        display = 0;
+        screen.textContent = display;
+        break;
+    case 'symbol plus':
+        operator = 'plus';
+        break;
+    case 'symbol minus':
+        operator = 'minus';
+        break;
+    case 'symbol multiply':
+        operator = 'multiply';
+        break;
+    case 'symbol divide':
+        operator = 'divide';
+        break;
+    case 'symbol equal':
+        operate(operator, first, second);
+        screen.textContent = display;
+        break;
+    }
+});
