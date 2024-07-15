4a10520dbbe2a668c8b83fce539713ad6463d10e
lizandreina <156568298+lizandreina@users.noreply.github.com>
2024-07-15 18:06:23 +0200

    Save final changes

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ let clickEvent = new Event('click');
 
 digits.forEach((button) => {
     button.addEventListener("click", (e) => {
-        if (display.length > 7) {
+        if (display.length > 5) {
             digits.disabled = true;
         } else {
             display.push(e.target.innerText);
